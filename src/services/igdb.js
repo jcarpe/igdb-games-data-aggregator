bc81577ec6fcb9234c3fd1a1bdfb3ee3bcddfbd5
Joe C <joe_carpenito@yahoo.com>
2022-01-20 22:10:36 -0500

    add parcel and start building out igdb service

--- src/services/igdb.js
+++ src/services/igdb.js
@@ -1,23 +1,85 @@
 import https from 'https'
+import { resolve } from 'path'
 
 export default class IGDB {
-  #clientID = ''
   #clientSecret = ''
+  #req = null
+  #baseReqConfig = {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' }
+  }
 
   /**
    * IGDB class constructor
    * @param {object} initParams initialization params for the service instance
-   * 
+   * initParams: {
+   *   clientSecret
+   *   clientID
+   *   baseURL
+   * }
    */
   constructor(initParams) {
-    this.#clientID = initParams.clientID
     this.#clientSecret = initParams.clientSecret
+    this.#baseReqConfig = {
+      ...this.#baseReqConfig,
+      headers: {
+        ...this.#baseReqConfig.headers,
+        'Client-ID': initParams.clientID
+      },
+      hostname: initParams.baseURL
+    }
+  }
+
+  authenticate(authURL) {
+    const options = {
+      ...this.#baseReqConfig,
+      hostname: authURL,
+      path: `/oauth2/token?client_id=${this.#baseReqConfig.clientID}&client_secret=${this.#baseReqConfig.clientSecret}&grant_type=client_credentials`,
+      headers: {
+        ...this.#baseReqConfig.headers,
+        'Content-Length': 0
+      }
+    }
+
+    const req = https.request(options, (res) => {
+      res.on('data', (data) => {
+       console.log(data)
+      })
+      res.on('error', (err) => {
+        console.log(err)
+      })
+    })
+    req.write('')
+    req.end();
   }
   
   /**
    * Start 
    */
-  request() {
-    return true
+  // request(endpoint, queryString) {
+  //   const options = {
+  //     ...this.#baseReqConfig,
+  //     headers: {
+  //       ...this.#baseReqConfig.headers,
+  //       'Content-Length': queryString.length
+  //     }
+  //   }
+
+  //   return new Promise ((resolve, reject) => {
+  //     const req = https.request(options, (res) => {
+  //       res.on('data', (data) => resolve(data))
+  //       res.on('error', (err) => reject(err))
+  //     })
+  //     req.write(queryString)
+  //     req.end();
+  //   }) 
+  // }
+
+  /**
+   * Allows inspection of the base configs for the HTTPS request
+   * @returns {object} baseRequestConfig 
+   */
+  readReqConfig() {
+    return Object.assign({}, this.#baseReqConfig)
   }
 }
--- src/services/igdb.spec.js
+++ src/services/igdb.spec.js
@@ -3,12 +3,20 @@ import IGDB from './igdb'
 describe('IGDB Service', () => {
   const igdbService = new IGDB({
     clientID: 'a-sample-client-ID',
-    clientSecret: 'a-sample-client-secret'
+    clientSecret: 'a-sample-client-secret',
+    baseURL: 'a-sample-base-url'
   });
 
   it('initializes service instance with config values', () => {
-    expect(igdbService.request()).toEqual(true)
+    expect(igdbService.readReqConfig()).toEqual({
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain', 'Client-ID': 'a-sample-client-ID' },
+      hostname: 'a-sample-base-url'
+    })
   })
-  it('generates an authenticated token on initialization', () => {})
+
+  it.skip('generates an authenticated token on initialization', () => {
+    console.log(igdbService.authenticate('id.twitch.tv'))
+  })
   it('makes successful request known request for full Super Mario Bros. games request', () => {})
 })
