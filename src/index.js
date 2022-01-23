ac12efda035ab0a9843c278fd20f83c1e924a519
Joe C <joe_carpenito@yahoo.com>
2022-01-23 14:18:54 -0500

    finally got full game request working! (dumb url mistake)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import IGDB from './services/igdb'
 
-const igdbService = new IGDB(
-  'e4ys2hra3fq94qxednr11me9ijuzos',
-  'rugsgs2huvr5b0p6xw274tu7nuyxyj',
-  'api.igdb.com'
-);
+const igdbService = new IGDB({
+  clientSecret: 'e4ys2hra3fq94qxednr11me9ijuzos',
+  clientID: 'rugsgs2huvr5b0p6xw274tu7nuyxyj',
+  baseURL: 'api.igdb.com'
+});
 
-igdbService.authenticate('id.twitch.tv')
-  .then(result => {
-    console.log(result)
-  })
-  .catch(err => {
-    console.error(err)
-  })
+const main = async () => {
+  await igdbService.authenticate('id.twitch.tv')
+
+  const result = await igdbService.request('v4/games', `
+    fields name;
+    search "breath of the wild";
+    limit 500;
+  `)
+
+  console.log(result)
+}
+
+main()
--- src/services/igdb.js
+++ src/services/igdb.js
@@ -1,16 +1,58 @@
 import https from 'https'
-import { resolve } from 'path'
+import zlib from 'zlib'
+import { Buffer } from 'buffer'
 
 export default class IGDB {
   #clientSecret = ''
   #req = null
   #baseReqConfig = {
     method: 'POST',
-    headers: { 'Content-Type': 'text/plain' }
+    headers: {
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'Content-Type': 'text/plain'
+    }
   }
 
   /**
-   * IGDB class constructor
+   * PRIVATE
+   * Generates a Promise-wrapped https request to be used agnostically
+   * @param {*} options The option object for the https request
+   * @param {String} queryString String formatted query to write to the request streamffffffffffffff
+   * @returns {Promise} The Promise-wrapped https request
+   */
+  #requestPromiseGenerator = async (options, queryString = '') => {
+    return new Promise((resolve, reject) => {
+      let buffer = []
+      const req = https.request(options, (res) => {
+        res.on('data', (data) => buffer.push(data))
+        res.on('error', (err) => reject(err))
+        res.on('end', () => resolve(Buffer.concat(buffer)))
+      })
+      req.write(queryString)
+      req.end();
+    })
+  }
+
+  /**
+   * PRIVATE
+   * Promise wrapped unzipper for gzipped responses
+   * @param {Array} buffer buffer array that is gzipped (usually API response)
+   * @returns Promise instance that will resolve an unzipped buffer or reject on error
+   */
+  #unzipPromiseGenerator = async buffer => {
+    // console.log(buffer)
+    return new Promise((resolve, reject) => {
+      zlib.unzip(buffer, (err, buffer) => {
+        if (err) reject(err)
+        else resolve(buffer)
+      })
+    })
+  }
+
+  /**
+   * IGDB class constructor with automatic authorization
    * @param {object} initParams initialization params for the service instance
    * initParams: {
    *   clientSecret
@@ -29,51 +71,53 @@ export default class IGDB {
       hostname: initParams.baseURL
     }
   }
-
-  authenticate(authURL) {
+  
+  /**
+   * Authenticate the app asynchronously
+   * @param {String} authURL url host used for authentication 
+   * @returns Promise instance that will resolve with auth response
+   */
+  async authenticate(authURL) {
     const options = {
       ...this.#baseReqConfig,
       hostname: authURL,
-      path: `/oauth2/token?client_id=${this.#baseReqConfig.clientID}&client_secret=${this.#baseReqConfig.clientSecret}&grant_type=client_credentials`,
+      path: `/oauth2/token?client_id=${this.#baseReqConfig.clientID}&client_secret=${this.#clientSecret}&grant_type=client_credentials`,
       headers: {
         ...this.#baseReqConfig.headers,
         'Content-Length': 0
       }
     }
 
-    const req = https.request(options, (res) => {
-      res.on('data', (data) => {
-       console.log(data)
-      })
-      res.on('error', (err) => {
-        console.log(err)
-      })
-    })
-    req.write('')
-    req.end();
-  }
-  
-  /**
-   * Start 
-   */
-  // request(endpoint, queryString) {
-  //   const options = {
-  //     ...this.#baseReqConfig,
-  //     headers: {
-  //       ...this.#baseReqConfig.headers,
-  //       'Content-Length': queryString.length
-  //     }
-  //   }
+    const result = await this.#requestPromiseGenerator(options)
 
-  //   return new Promise ((resolve, reject) => {
-  //     const req = https.request(options, (res) => {
-  //       res.on('data', (data) => resolve(data))
-  //       res.on('error', (err) => reject(err))
-  //     })
-  //     req.write(queryString)
-  //     req.end();
-  //   }) 
-  // }
+    this.#baseReqConfig = {
+      ...this.#baseReqConfig,
+      headers: {
+        ...this.#baseReqConfig.headers,
+        'Authorization': `Bearer ${JSON.parse(result)['access_token']}`
+      }
+    }
+
+    return result
+  }
+
+  /**
+   * Request 
+   */
+  async request(endpoint, queryString) {
+    const options = {
+      ...this.#baseReqConfig,
+      path: `/${endpoint}`,
+      headers: {
+        ...this.#baseReqConfig.headers,
+        'Content-Length': queryString.length
+      }
+    }
+
+    const result = await this.#requestPromiseGenerator(options, queryString)
+    const unzipped = await this.#unzipPromiseGenerator(result)
+    return unzipped.toString('utf8')
+  }
 
   /**
    * Allows inspection of the base configs for the HTTPS request
