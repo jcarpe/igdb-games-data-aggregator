7862398d7cd4f7aa259b074645aee7fa6e2dea8a
Joe C <joe_carpenito@yahoo.com>
2022-01-25 21:28:56 -0500

    foundation for CLZ data parsing

---
+++ src/utils/clz-game-collector-import.spec.js
@@ -0,0 +1,9 @@
+describe('CLZ Game Collector Import', () => {
+  it('loads XML data exported from CLZ app and transforms to JSON', () => {
+
+  })
+
+  it('allows transform/distillation of just an array of all game titles', () => {
+
+  })
+})
