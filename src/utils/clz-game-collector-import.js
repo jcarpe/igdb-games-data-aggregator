a752d7cabb60ba6745c38ef510f18c58c697553c
Joe C <joe_carpenito@yahoo.com>
2022-01-27 21:12:35 -0500

    clz library games data parser

---
+++ src/utils/clz-game-collector-import.js
@@ -0,0 +1,16 @@
+import { readFile } from 'fs/promises'
+import { XMLParser } from 'fast-xml-parser'
+
+export const loadGames = async (filePath) => {
+  const parser = new XMLParser()
+
+  const xmlData = await readFile(filePath, { encoding: 'utf8' })
+  const jsonData = parser.parse(xmlData)
+  const titleArr = []
+
+  jsonData.gameinfo.gamelist.game.forEach((game) => {
+    titleArr.push(game.title)
+  })
+
+  return titleArr
+}
--- src/utils/clz-game-collector-import.spec.js
+++ src/utils/clz-game-collector-import.spec.js
@@ -1,9 +1,16 @@
+import { loadGames } from './clz-game-collector-import'
+
 describe('CLZ Game Collector Import', () => {
-  it('loads XML data exported from CLZ app and transforms to JSON', () => {
-
-  })
-
-  it('allows transform/distillation of just an array of all game titles', () => {
-
+  it('loads XML data exported from CLZ app returns array of titles', async () => {
+    const games = await loadGames(`./__mocks__/clz-games-data.xml`)
+    expect(games).toEqual([
+      '1Xtreme (Greatest Hits)',
+      '8 Eyes',
+      'Adventure',
+      'Albert Odyssey: Legend of Eldean',
+      'Alisia Dragoon',
+      'Arcade Classic No. 4: Defender / Joust',
+      'Armored Core 3'
+    ])
   })
 })
