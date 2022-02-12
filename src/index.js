bfab65d73f4aaa0dd5297871a16ed7cd4f2ce413
Joe C <joe_carpenito@yahoo.com>
2022-02-12 13:59:08 -0500

    playing with batched requests

--- src/index.js
+++ src/index.js
@@ -8,25 +8,52 @@ const igdbService = new IGDB({
   baseURL: process.env.SERVICE_BASE_URL
 });
 
+const multiBatchRequester = async (gameBatches) => {
+  let allGames = []
+
+  const batchRequest = async (titleArr) => {
+    const igdbData = await igdbService.request(
+      'multiquery',
+      generateGamesMultiQuery(titleArr, ['name','platforms.name'])
+    )
+
+    return igdbData[0].result
+  }
+
+  gameBatches.forEach(async (gameBatch) => {
+    let titleArr = []
+    gameBatch.forEach((game) => titleArr.push(game.title))
+
+    const batchData = await batchRequest(titleArr)
+    console.log(batchData)
+    allGames = allGames.concat(allGames, batchData)
+  })
+
+  return allGames
+}
+
 const main = async () => {
   /**
    * TODO:
    * This is operating as a bit of a playground at the moment...
    */
   await igdbService.authenticate(process.env.SERVICE_O_AUTH_URL)
-  const games = await loadGames('./__mocks__/clz-games-data.xml')
+  const games = await loadGames('./__mocks__/clz-games-data-FULL.xml')
+  const gameData = await multiBatchRequester(games)
 
-  let titleArr = []
-  games.forEach((game) => titleArr.push(game.title))
+  console.log(gameData)
 
-  console.log(games)
+  // let titleArr = []
+  // games[0].forEach((game) => titleArr.push(game.title))
 
-  const igdbData = await igdbService.request(
-    'multiquery',
-    generateGamesMultiQuery(titleArr, ['name','platforms.name'])
-  )
+  // console.log(titleArr)
 
-  console.log(JSON.stringify(igdbData))
+  // const igdbData = await igdbService.request(
+  //   'multiquery',
+  //   generateGamesMultiQuery(titleArr, ['name','platforms.name'])
+  // )
+
+  // console.log(JSON.stringify(igdbData))
 }
 
 main()
