f5fe35a7666b2165d032383e570e4f6ed69690df
Joe C <joe_carpenito@yahoo.com>
2022-07-18 22:25:15 -0400

    add system name mapping (crude)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { loadGames } from './utils/clz-game-collector-import'
 import IGDB from './services/igdb'
 import { generateGamesMultiQuery } from './utils/request-generators';
 
+const systemNameMap = require('./data-maps/system-names.json')
+
 const igdbService = new IGDB({
   clientID: process.env.SERVICE_CLIENT_ID,
   clientSecret: process.env.SERVICE_CLIENT_TOKEN,
@@ -54,12 +56,38 @@ const main = async () => {
   /**
    * TODO:
    * This is operating as a bit of a playground at the moment...
+   * 
+   * A mess of experimentation requiring refactor
    */
   await igdbService.authenticate(process.env.SERVICE_O_AUTH_URL)
-  const games = await loadGames('./__mocks__/clz-games-data-FULL.xml')
+
+  const games = await loadGames('./__mocks__/clz-games-data.xml')
   const gameData = await multiBatchRequester(games)
 
-  writeFile('./dist/igdb-data.json', JSON.stringify(gameData))
+  const clzGames = games.flat();
+  
+  let mushedData = [];
+  clzGames.forEach(clzGame => {
+    let platformOwned = null;
+    let matchedGamesPlatform = gameData.find(el => {
+      return el.platforms?.find( platform => {
+        const systemNameGroup = systemNameMap.find(nameGroup => nameGroup.igdbKey === platform.name)
+        if (systemNameGroup.names.includes(clzGame.platform)) {
+          platformOwned = platform;
+          return true
+        }
+
+        return false
+      })
+    })
+
+    if (matchedGamesPlatform) {
+      matchedGamesPlatform.platform_owned = platformOwned;
+      mushedData.push(matchedGamesPlatform)
+    }
+  })
+
+  writeFile('./dist/igdb-data.json', JSON.stringify(mushedData))
 }
 
 main()
